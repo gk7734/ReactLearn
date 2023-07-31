a672f622af9ff2ebcb767bdcbd39a843317eafb2
gk7734 <gamekiller7734@naver.com>
2023-07-31 16:13:21 +0900

    add to layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import {Route, Routes} from 'react-router-dom';
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import Favorites from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
   return (
     <div>
-
+        <MainNavigation />
+        <Routes>
+            <Route path='/' element={<AllMeetupsPage />}/>
+            <Route path='/new-meetup' element={<NewMeetupPage />}/>
+            <Route path='/favorites' element={<Favorites />}/>
+        </Routes>
     </div>
   );
 }
