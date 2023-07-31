ed06b919d00f2521b36c32f2828abbe04f9b068d
gk7734 <gamekiller7734@naver.com>
2023-07-31 17:07:11 +0900

    meetups component complete

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -4,7 +4,7 @@ import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
     return <header className={classes.header}>
-        <div className={classes.logo}>React Meetups</div>
+        <div className={classes.logo}>GK Meetups</div>
         <nav>
             <ul>
                 <li>
