0a6d7319ce76786074cfbc6374a05d2ab083ed2c
gk7734 <gamekiller7734@naver.com>
2023-08-01 01:16:11 +0900

    Firebase POST 날리기

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,22 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    function addMeetupHandler(meetupData) {
+        fetch('https://react-study-gk7734-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
         </section>
     )
 }
