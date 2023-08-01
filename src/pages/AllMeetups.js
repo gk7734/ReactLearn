c8ce1d433e3645742ef85891da575190e2303a88
gk7734 <gamekiller7734@naver.com>
2023-08-01 14:13:53 +0900

    useEffect 사용

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,38 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import MeetupList from "../components/meetups/MeetupList";
 import {isRouteErrorResponse} from "react-router-dom";
 import Loading from "../components/ui/Loading";
 
-const DUMMY_DATA = [
-    {
-        id: 'm1',
-        title: 'This is a first meetup',
-        image:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-        id: 'm2',
-        title: 'This is a second meetup',
-        image:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-];
 function AllMeetupsPage() {
     const [isLoading, setIsLoading] = useState(false);
-    const [loadedMeetups, setLoadedMeetups] = useState();
-    fetch('https://react-study-gk7734-default-rtdb.firebaseio.com/meetups.json',
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    useEffect(() =>{
+        setIsLoading(true);
+        fetch('https://react-study-gk7734-default-rtdb.firebaseio.com/meetups.json',
         ).then(response =>{
             return  response.json();
-    }).then(data =>{
-        setIsLoading(true);
-    })
+        }).then(data =>{
+            const meetups = [];
+
+            for (const key in data) {
+                const meetup ={
+                    id: key,
+                    ...data[key]
+                };
+
+                meetups.push(meetup);
+            }
+
+            setIsLoading(false);
+            setLoadedMeetups(meetups);
+        })
+    }, []);
+
+
 
     if (isLoading) {
         return <section>
@@ -42,7 +40,7 @@ function AllMeetupsPage() {
 
     return <section>
         <h1>All Meetups</h1>
-        <MeetupList meetups={DUMMY_DATA}/>
+        <MeetupList meetups={loadedMeetups}/>
     </section>
 }
 
